71d718410d5c3105fe85973f47cb876ca1e40d3c
shtabdeepaull <shtabdee30@gmail.com>
2022-05-12 10:12:16 +0600

    added information

---
+++ src/Pages/Home/Services.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div className='my-28'>
+            <div className='text-center'>
+                <h3 className='text-primary'>Our Services</h3>
+                <h1>Services We Provide</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
