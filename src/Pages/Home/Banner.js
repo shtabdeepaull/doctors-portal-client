cc3cedae4576e9a456068c132f48b81fdf81a790
shtabdeepaull <shtabdee30@gmail.com>
2022-05-14 11:35:53 +0600

    load data from mongodb

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import chair from "../../assets/images/chair.png";
+import bg from '../../assets/images/bg.png';
 import PrimaryButton from '../Shared/PrimaryButton';
 
 
 const Banner = () => {
     return (
         <div>
-            <div className="hero min-h-screen">
+            <div style={{
+                background: `url(${bg})`,
+                backgroundSize: 'cover'
+            }}
+            className="hero min-h-screen">
                 <div className="hero-content flex-col lg:flex-row-reverse">
                     <img src={chair} className="max-w-sm rounded-lg shadow-2xl" />
                     <div>
